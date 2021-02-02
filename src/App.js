2f9f4a01fff80bd2b5ef17729938cbc7e41b51b3
Administrator <cesareo.zavala@gmail.com>
2021-02-01 19:04:19 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          i was YOLO <code>src/App.js</code> and ces  save to reload.
         </p>
         <a
           className="App-link"
